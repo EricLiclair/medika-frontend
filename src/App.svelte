dcdb2f52e3570c9b2e59a4161ae7db20b5becf63
diliga8356 <diliga8356@nahetech.com>
2022-01-22 05:10:18 -0500

    init commit

---
+++ src/App.svelte
@@ -0,0 +1,55 @@
+<script>
+    import TableRow from './TableRow.svelte';
+	let products=[];
+    let url = 'https://port-8000-mdkbzr-diliga8356426146.codeanyapp.com/api/v1/search/Atriplex/';
+    fetch(url).then((res) => res.json()).then((data) => {
+        console.log(data)
+        products = [...data]
+    })
+</script>
+
+<main>
+    <table style="text-align: left; border: solid 1px black;">
+        <tr>
+            <th>Name</th>
+            <th>Price</th>
+            <th>Quantity</th>
+            <th>Tools</th>
+        </tr>
+    {#each products as product (product.id)}
+        <TableRow {product}/>
+    {/each}
+    </table>
+</main>
+
+<style>
+    table {
+    font-family: Arial, Helvetica, sans-serif;
+    border-collapse: collapse;
+    width: 60%;
+    }
+
+    table th {
+    border: 1px solid #ddd;
+    padding: 8px;
+    }
+
+    table th {
+    padding-top: 12px;
+    padding-bottom: 12px;
+    text-align: left;
+    background-color: #04AA6D;
+    color: white;
+    }
+	main {
+		text-align: center;
+		padding: 1em;
+		margin: 0 auto;
+	}
+
+	@media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
+	}
+</style>
---
+++ src/TableRow.svelte
@@ -0,0 +1,58 @@
+<script>
+    export let product = {
+        name: 'Product Name',
+        price: 100.00,
+        quantity: 100
+    }
+
+    let updatedProduct = {
+        name: "hello",
+        price: null,
+        quantity: null,
+    }
+
+    let editable = false;
+    const toggle_editable = () => {
+        editable = !editable;
+    }
+
+    const updateProduct = (e, type) => {
+        switch(type) {
+            case 'name':
+                updatedProduct.name = e.target.value;
+                break;
+            case 'price':
+                // code block
+                break;
+            case 'quantity':
+                // code block
+                break;
+            }
+        
+    }
+</script>
+
+<tr on:click={() => {editable = true;}}>
+    <td on:input={(e) => updateProduct(e,'name')} contenteditable={editable}>{product.name}</td>
+    <td contenteditable={editable}>{product.price}</td>
+    <td contenteditable={editable}>{product.quantity}</td>
+    {#if editable}
+        <td ><button>save</button><button on:click={(e) => {e.stopPropagation(); toggle_editable();}}>cancel</button></td>
+    <!-- {:else}
+        <td ><button>edit</button></td> -->
+    {/if}
+    <td>{updatedProduct.name}</td>
+</tr>
+
+<style>
+    button {
+        margin: 10px;
+    }
+    tr {
+    border: 1px solid #ddd;
+    }
+
+    tr:nth-child(even){background-color: #f2f2f2;}
+
+    tr:hover {background-color: #ddd;}
+</style>
